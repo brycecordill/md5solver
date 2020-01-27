07bbec7616b582749e00d4c8bcb95f26a482dc9a
Bryce Cordill <brycecordill@gmail.com>
2020-01-26 20:22:37 -0800

    First threaded version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate rand;
 extern crate md5;
-use std::{env, process};
+use std::{env, process, thread};
 use rand::{Rng, distributions::Alphanumeric};
+use std::sync::mpsc;
+
+static NUM_THREAD: i32 = 8;
+static STR_SIZE: usize = 10;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,10 +21,37 @@ fn main() {
 
     println!("Computing MD5 hashes that start with: {}", to_search);
 
-    loop {
+    let mut workers = vec![];
+    let mut comms = vec![];
+    let (main_tx, main_rx) = mpsc::channel();
+
+    for _i in 0..NUM_THREAD {
+        let temp_str = to_search.clone();
+        let temp_main_tx = main_tx.clone();
+        let (temp_tx, temp_rx) = mpsc::channel();
+        comms.push(temp_tx);
+        workers.push(thread::spawn(move || {
+            bruteforce_md5(&temp_str, temp_rx);
+            temp_main_tx.send(true).unwrap();
+        }));
+    }
+
+    main_rx.recv().unwrap();
+
+    for tx in comms {
+        tx.send(true).ok();
+    }
+
+    for worker in workers {
+        let _ = worker.join();
+    }
+}
+
+fn bruteforce_md5(to_search: &str, rx: mpsc::Receiver<bool>) {
+    while rx.try_recv().is_err() {
         let rand_string = rand::thread_rng()
             .sample_iter(&Alphanumeric)
-            .take(10)
+            .take(STR_SIZE)
             .collect::<String>();
 
         let digest = md5::compute(&rand_string);
