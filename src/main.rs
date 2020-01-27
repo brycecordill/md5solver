50ee832937b62f297ef538a0d1a5cebb2047c927
Bryce Cordill <brycecordill@gmail.com>
2020-01-26 18:15:49 -0800

    First working version

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,19 @@ fn main() {
         None => process::exit(1),
     };
 
-    println!("Search: {}", to_search);
+    println!("Searching for MD5 hashes that start with: {}", to_search);
 
-    let rand_string = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(15)
-        .collect::<String>();
+    loop {
+        let rand_string = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(15)
+            .collect::<String>();
 
-    println!("Rand_string: {}", rand_string);
-
-    let digest = md5::compute(rand_string);
-    let as_hex = format!("{:x}", digest);
-    println!("MD5: {}", as_hex);
+        let digest = md5::compute(&rand_string);
+        let as_hex = format!("{:x}", digest);
+        
+        if as_hex.starts_with(to_search) {
+            println!("String: {} \t MD5: {}", rand_string, as_hex);
+        }
+    }
 }
